76ba3b001f2a324b0dbd47e8cdd8f82baefe8984
Upender5119 <105918338+Upender5119@users.noreply.github.com>
2024-10-24 12:57:19 +0530

    Update main.tf

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -2,4 +2,8 @@ resource "aws_cloudtrail" "insecure-logging" {
   name           = "cloudtrail-logging"
   s3_bucket_name = "my-cloudtrail-bucket"
   enable_logging = true
+  enable_log_file_validation = true
+  kms_key_id = var.kms_key_id
+  is_multi_region_trail =  true
+  cloud_watch_logs_group_arn = var.cloud_watch_logs_group_arn 
 }
